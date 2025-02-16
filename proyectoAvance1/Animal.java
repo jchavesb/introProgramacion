45e5784d1f2200a8593cab3949bd2ff276c69200
Jairo Chaves Brenes <jchaves60919@ufide.ac.cr>
2025-02-16 10:41:29 -0500

    arreglando mis clases Animal, Zoologico. Agregando espacios entre metodos para Ubicacion

--- proyectoAvance1/Animal.java
+++ proyectoAvance1/Animal.java
@@ -1,27 +1,45 @@
+/*
+ * @authors Jairo Chaves Brenes, Jessica Montero Obando, Wilson Blanco Hernandez
+ * Date: Febrero 16, 2025
+ * Proyecto Final - 1er adelanto.
+ */
+
 package com.mycompany.proyectofinaladelanto1;
 
 public class Animal {
     private String nombre;
     private String especie;
     private int edad;
-    
-    public void Animal(String nombre, String especie, int edad) {
-        // Constructor
-    }
 
-    public void String getNombre() {
-       // Método para obtener nombre del animal
-    }
-
-    public  void String getEspecie() {
-        // Método para obtener la especie de animal
-    }
-
-    public int getEdad() {
-        // Método para obtener la especie de animal
+    public void setNombre( String nombre) {
+        // Método para agregar el nombre de un animal
     }
     
-    public int setEdad() {
-        // Método para obtener la edad del animal
+    public void getNombre() {
+        // Método para obtener el nombre de un animal
     }
+    
+    public void setEspecie() {
+        // Método para agregar la especie de un animal
+    }
+    
+    public void getEspecie() {
+        // Método para obtener la especie de un animal
+    }
+    
+    public void setEdad(int edad) {
+        // Método para agregar la edad de un animal
+    }
+    
+    public void getEdad() {
+        // Método para obtener la edad de un animal
+    }
+    
+    //public void toString() {
+        // Revisar este metodo, al ser declarado muestra el error:
+        // toString() in Animal cannot override toString() in Object
+        // return type void is not compatible with String
+    //}
+    
+    
 }
--- proyectoAvance1/Ubicacion.java
+++ proyectoAvance1/Ubicacion.java
@@ -1,28 +1,39 @@
+/*
+ * @authors Jairo Chaves Brenes, Jessica Montero Obando, Wilson Blanco Hernandez
+ * Date: Febrero 16, 2025
+ * Proyecto Final - 1er adelanto.
+ */
+
 package com.mycompany.proyectofinaladelanto1;
 
-/**
- *
- * @author wilsonblanco
- */
 public class Ubicacion {
 
     private String bioma;
     private int posicion;
     private Animal[] animales;
+    
     // Atributos de clase
     public void setBioma(String bioma){
 
-    } // Metodo para elegir un bioma.
+    } 
+
+    // Metodo para elegir un bioma.
     public String getBioma(){
 
-    } // Metodo para obtener el bioma elegido.
+    } 
+
+    // Metodo para obtener el bioma elegido.
     public void setPosicion(){
 
-    } // Metodo para elegir una posicion para el animal
+    } 
+
+    // Metodo para elegir una posicion para el animal
     public int getPosicion(){
 
-    } // Metodo para obtener la posicion
+    } 
+
+    // Metodo para obtener la posicion
     public void getAnimales(){
     } // Metodo para obtener los animales dentro de la ubicacion.
 
-} // Clase ubica
+}
